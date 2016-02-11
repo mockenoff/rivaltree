09119af66e3b444bff421ba9ba06912b7bd70e61
mockenoff <mockenoff@yahoo.com>
2016-02-10 19:18:30 -0600

    Do the redirect on new saved bracket

--- tourf/app/pods/brackets/fresh/controller.js
+++ tourf/app/pods/brackets/fresh/controller.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	isSubmitting: false,
+	canSubmit: Ember.computed('isSubmitting', 'model.title', 'model.teams.[]', function() {
+		return this.get('isSubmitting') === true || (this.get('model.title') || '').trim() === '' || this.get('model.teams').length === 0 ? false : true;
+	}),
+
+	actions: {
+		saveBracket() {
+			this.set('isSubmitting', true);
+			this.get('model').save().then(Ember.run.bind(this, function(data) {
+				this.set('isSubmitting', false);
+				this.transitionTo('brackets.view', this.model.get('id'));
+			})).catch(Ember.run.bind(this, function(err) {
+				this.set('isSubmitting', false);
+			}));
+		},
+	},
 });
