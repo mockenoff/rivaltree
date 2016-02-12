2d163abf001452022c00727cdf735b52392b842e
Tim Poon <mockenoff@yahoo.com>
2016-02-11 21:42:35 -0600

    Simplify lifecycle hooks on instant-check

--- tourf/app/pods/components/instant-check/component.js
+++ tourf/app/pods/components/instant-check/component.js
@@ -11,30 +11,21 @@ export default Ember.Component.extend({
 
 	didInsertElement() {
 		this._super(...arguments);
-
-		var inputElement = this.element.querySelector('input[type="checkbox"]');
-		this.set('inputElement', inputElement);
-		this.element.querySelector('label').setAttribute('for', inputElement.id);
-
-		this.set('changeEvent', Ember.run.bind(this, this.get('changeEvent')));
-		inputElement.addEventListener('change', this.get('changeEvent'));
+		this.set('inputElement', this.element.querySelector('input[type="checkbox"]'));
 	},
 
-	willDestroyElement() {
-		this._super(...arguments);
-		this.get('inputElement').removeEventListener('change', this.get('changeEvent'));
-	},
+	actions: {
+		changeEvent() {
+			this.set('value', this.get('inputElement').checked);
 
-	changeEvent() {
-		this.set('value', this.get('inputElement').checked);
-
-		if (this.get('alwaysSave') === true) {
-			this.set('isSubmitting', true);
-			this.get('model').save().then(Ember.run.bind(this, function() {
-				this.set('isSubmitting', false);
-			})).catch(Ember.run.bind(this, function(err) {
-				this.set('isSubmitting', false); // BUG: show error feedback
-			}));
-		}
+			if (this.get('alwaysSave') === true) {
+				this.set('isSubmitting', true);
+				this.get('model').save().then(Ember.run.bind(this, function() {
+					this.set('isSubmitting', false);
+				})).catch(Ember.run.bind(this, function(err) {
+					this.set('isSubmitting', false); // BUG: show error feedback
+				}));
+			}
+		},
 	},
 });
